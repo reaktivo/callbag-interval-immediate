83736d125e43eb341fdb839aa6b1430b3f319cd3
Marcel Miranda Ackerman <m@reaktivo.com>
2021-06-01 14:04:04 +0000

    Removed jest and jest dependencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,12 @@
-import { forEach, pipe, take } from "callbag-basics";
-import interval from "./index";
+import * as bt from 'baretest';
+import * as assert from 'assert';
+import { forEach, pipe, take } from 'callbag-basics';
+import interval from './index';
 
-describe("interval immediate", () => {
-  it("should emit immediately", done => {
+const test = bt('interval immediate');
+
+test('should emit immediately', async () => {
+  return new Promise(resolve => {
     const values = [];
     pipe(
       interval(1), // milliseconds
@@ -13,11 +17,14 @@ describe("interval immediate", () => {
           // 0 synchronous value
           // 999 added manually after interval
           // 1 added at next tick
-          expect(values).toEqual([0, 999, 1]);
-          done();
+          // expect(values).toEqual([0, 999, 1]);
+          assert.deepStrictEqual(values, [0, 999, 1]);
+          resolve(undefined);
         }
       })
     );
     values.push(999);
   });
 });
+
+test.run();
